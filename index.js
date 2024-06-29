0f47cf86bd483ba4dcaaaa0731d9e19f1f9a5d48
Vishwas JS <vishwasjs2806@gmail.com>
2024-06-29 11:55:58 +0530

    latest

--- index.js
+++ index.js
@@ -64,43 +64,60 @@ var swiper = new Swiper(".cement-container", {
 
 
   // ant tab code
-  const stepsFilter = document.querySelector(".steps-filter");
-  const stepsItem = document.querySelectorAll(".steps-item");
-  
-  // Add 'active' class to the first filter item initially
-  const stepsFirstFilter = stepsFilter.querySelector(".filter-item");
-  stepsFirstFilter.classList.add("active");
-  
-  // Get the filter value of the first filter item
-  const stepsFirstValue = stepsFirstFilter.getAttribute("data-filter");
-  
-  // Show items corresponding to the first filter value
-  stepsItem.forEach(item => {
-      if (item.classList.contains(libraryFirstValue) || libraryFirstValue === 'all') {
-          item.classList.remove("hide");
-          item.classList.add("show");
-      } else {
-          item.classList.remove("show");
-          item.classList.add("hide");
-      }
-  });
-  
-  // Event listener for filter items
-  stepsFilter.addEventListener("click", (event) => {
-      if (event.target.classList.contains("filter-item")) { // deactivate existing active 'filter-item'
-          libraryFilter.querySelector(".active").classList.remove("active");
-          // activate new 'filter-item'
-          event.target.classList.add("active");
-          const filterValue = event.target.getAttribute("data-filter");
-          libraryItem.forEach((item) => {
-              if (item.classList.contains(filterValue) || filterValue === 'all') {
-                  item.classList.remove("hide");
-                  item.classList.add("show");
-              } else {
-                  item.classList.remove("show");
-                  item.classList.add("hide");
-              }
-          });
-      }
-  });
-  
+// ant design for library section
+// Library filter
+const stepsFilter = document.querySelector(".steps-filter");
+const stepsItem = document.querySelectorAll(".steps-item");
+
+// Add 'active' class to the first filter item initially
+const stepsFirstFilter = stepsFilter.querySelector(".filter-item");
+stepsFirstFilter.classList.add("active");
+
+// Get the filter value of the first filter item
+const stepsFirstValue = stepsFirstFilter.getAttribute("data-filter");
+
+// Show items corresponding to the first filter value
+stepsItem.forEach(item => {
+    if (item.classList.contains(stepsFirstValue) || stepsFirstValue === 'all') {
+        item.classList.remove("hide");
+        item.classList.add("show");
+    } else {
+        item.classList.remove("show");
+        item.classList.add("hide");
+    }
+});
+
+// Event listener for filter items
+stepsFilter.addEventListener("click", (event) => {
+    if (event.target.classList.contains("filter-item")) { // deactivate existing active 'filter-item'
+        stepsFilter.querySelector(".active").classList.remove("active");
+        // activate new 'filter-item'
+        event.target.classList.add("active");
+        const filterValue = event.target.getAttribute("data-filter");
+        stepsItem.forEach((item) => {
+            if (item.classList.contains(filterValue) || filterValue === 'all') {
+                item.classList.remove("hide");
+                item.classList.add("show");
+            } else {
+                item.classList.remove("show");
+                item.classList.add("hide");
+            }
+        });
+    }
+});
+
+// faqq
+function toggleAnswer(element) {
+  const answer = element.nextElementSibling;
+  const toggleSymbol = element.querySelector('.faq-toggle');
+
+  if (answer.style.maxHeight == '100%') {
+      answer.style.maxHeight = '0';
+      answer.style.padding = '0';
+      toggleSymbol.textContent = '+';
+  } else {
+      answer.style.maxHeight = '100%';
+      answer.style.padding = '30px';
+      toggleSymbol.textContent = '-';
+  }
+}
