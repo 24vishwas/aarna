ae2c9418427261cef6da992e290bc5618d411406
Vishwas JS <vishwasjs2806@gmail.com>
2024-07-22 21:17:37 +0530

    first commit

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ var swiper = new Swiper(".cement-container", {
         delay: 1500,
         disableOnInteraction: false,
       },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+   
     breakpoints: {
         // when window width is >= 320px
         320: {
